25c26b8310cabea7ee82f379354fcccb6690d67b
Yi donghoon <icq4ever@gmail.com>
2020-02-06 22:41:42 +0900

    ui done

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -5,26 +5,24 @@ void ofApp::setup() {
 	// dmx.connect("tty.usbserial-EN143965"); // use the name
 	dmx.connect(0, 128); // or use a number
     
-    chan1.set("Channel 1", 0, 0, 255);
-    chan2.set("Channel 2", 0, 0, 255);
-    chan3.set("Channel 3", 0, 0, 255);
-    chan4.set("Channel 4", 0, 0, 255);
-    chan5.set("Channel 5", 0, 0, 255);
-    chan6.set("Channel 6", 0, 0, 255);
-    chan7.set("Channel 7", 0, 0, 255);
-    chan8.set("Channel 8", 0, 0, 255);
-    chan9.set("Channel 9", 0, 0, 255);
-    chan10.set("Channel 10", 0, 0, 255);
-    chan11.set("Channel 11", 0, 0, 255);
-    chan12.set("Channel 12", 0, 0, 255);
-    chan13.set("Channel 13", 0, 0, 255);
-    chan14.set("Channel 14", 0, 0, 255);
-    chan15.set("Channel 15", 0, 0, 255);
-    chan16.set("Channel 16", 0, 0, 255);
+    dmxID.set("LIGHT_ID", 1, 1, 4);
+    chan1.set ("x", 0, 0, 255);
+    chan2.set ("xFine", 0, 0, 255);
+    chan3.set ("y", 0, 0, 255);
+    chan4.set ("yFine", 0, 0, 255);
+    chan5.set ("xySpeed", 0, 0, 255);
+    chan6.set ("Dimmer", 0, 0, 255);
+    chan7.set ("strobe", 0, 0, 255);
+    chan8.set ("r", 0, 0, 255);
+    chan9.set ("g", 0, 0, 255);
+    chan10.set("b", 0, 0, 255);
+    chan11.set("w", 0, 0, 255);
+    chan12.set("zoom", 0, 0, 255);
     autoCycle.set("AutoCycle", false);
     
     panel.setName("Panel");
     panel.setup();
+    panel.add(dmxID);
     panel.add(chan1);
     panel.add(chan2);
     panel.add(chan3);
@@ -37,11 +35,6 @@ void ofApp::setup() {
     panel.add(chan10);
     panel.add(chan11);
     panel.add(chan12);
-    panel.add(chan13);
-    panel.add(chan14);
-    panel.add(chan15);
-    panel.add(chan16);
-    panel.add(autoCycle);
 }
 
 void ofApp::update() {
@@ -67,54 +60,21 @@ void ofApp::update() {
     // }
 
     // change CH number
-    int margin = 0;
-    margin = 48;
-	dmx.setLevel(margin + 1, chan1);
-	dmx.setLevel(margin + 2, chan2);
-    dmx.setLevel(margin + 3, chan3);
-    dmx.setLevel(margin + 4, chan4);
-    dmx.setLevel(margin + 5, chan5);
-    dmx.setLevel(margin + 6, chan6);
-    dmx.setLevel(margin + 7, chan7);
-	dmx.setLevel(margin + 8, chan8);
-    dmx.setLevel(margin + 9, chan9);
-    dmx.setLevel(margin + 10, chan10);
-    dmx.setLevel(margin + 11, chan11);
-    dmx.setLevel(margin + 12, chan12);
-    dmx.setLevel(margin + 13, chan13);
-	dmx.setLevel(margin + 14, chan14);
-    dmx.setLevel(margin + 15, chan15);
-    dmx.setLevel(margin + 16, chan16);
-	dmx.update();
+	dmx.setLevel(dmxID * 16 + 1, chan1);
+	dmx.setLevel(dmxID * 16 + 2, chan2);
+    dmx.setLevel(dmxID * 16 + 3, chan3);
+    dmx.setLevel(dmxID * 16 + 4, chan4);
+    dmx.setLevel(dmxID * 16 + 5, chan5);
+    dmx.setLevel(dmxID * 16 + 6, chan6);
+    dmx.setLevel(dmxID * 16 + 7, chan7);
+	dmx.setLevel(dmxID * 16 + 8, chan8);
+    dmx.setLevel(dmxID * 16 + 9, chan9);
+    dmx.setLevel(dmxID * 16 + 10, chan10);
+    dmx.setLevel(dmxID * 16 + 11, chan11);
+    dmx.setLevel(dmxID * 16 + 12, chan12);
+    dmx.update();
 }
 
 void ofApp::draw() {
-
-	ofSetColor(chan1);
-	ofDrawRectangle(0, 0, ofGetWidth() / 3, ofGetHeight()/2);
-	
-	ofSetColor(chan2);
-	ofDrawRectangle(ofGetWidth() / 3, 0, ofGetWidth() / 3, ofGetHeight()/2);
-    
-    ofSetColor(chan3);
-    ofDrawRectangle(2*(ofGetWidth() / 3), 0, ofGetWidth() / 3, ofGetHeight()/2);
-
-    ofSetColor(chan4);
-    ofDrawRectangle(0*(ofGetWidth() / 3), ofGetHeight()/2, ofGetWidth() / 3, ofGetHeight());
-    ofSetColor(chan5);
-    ofDrawRectangle((ofGetWidth() / 3), ofGetHeight()/2, ofGetWidth() / 3, ofGetHeight());
-    ofSetColor(chan6); ofDrawRectangle(2*(ofGetWidth() / 3), ofGetHeight()/2, ofGetWidth() / 3, ofGetHeight());
-    ofSetColor(chan7); ofDrawRectangle(2*(ofGetWidth() / 3), ofGetHeight()/2, ofGetWidth() / 3, ofGetHeight());
-    ofSetColor(chan8); ofDrawRectangle(2*(ofGetWidth() / 3), ofGetHeight()/2, ofGetWidth() / 3, ofGetHeight());
-    ofSetColor(chan9); ofDrawRectangle(2*(ofGetWidth() / 3), ofGetHeight()/2, ofGetWidth() / 3, ofGetHeight());
-    ofSetColor(chan10); ofDrawRectangle(2*(ofGetWidth() / 3), ofGetHeight()/2, ofGetWidth() / 3, ofGetHeight());
-    ofSetColor(chan11); ofDrawRectangle(2*(ofGetWidth() / 3), ofGetHeight()/2, ofGetWidth() / 3, ofGetHeight());
-    ofSetColor(chan12); ofDrawRectangle(2*(ofGetWidth() / 3), ofGetHeight()/2, ofGetWidth() / 3, ofGetHeight());
-    ofSetColor(chan13); ofDrawRectangle(2*(ofGetWidth() / 3), ofGetHeight()/2, ofGetWidth() / 3, ofGetHeight());
-    ofSetColor(chan14); ofDrawRectangle(2*(ofGetWidth() / 3), ofGetHeight()/2, ofGetWidth() / 3, ofGetHeight());
-    ofSetColor(chan15); ofDrawRectangle(2*(ofGetWidth() / 3), ofGetHeight()/2, ofGetWidth() / 3, ofGetHeight());
-    ofSetColor(chan16); ofDrawRectangle(2*(ofGetWidth() / 3), ofGetHeight()/2, ofGetWidth() / 3, ofGetHeight());
-    
-    
     panel.draw();
 }
--- src/ofApp.h
+++ src/ofApp.h
@@ -14,22 +14,23 @@ public:
 	int level;
     
     ofxPanel panel;
-    ofParameter<int> chan1;
-    ofParameter<int> chan2;
-    ofParameter<int> chan3;
+	ofParameter<int> dmxID;
+	ofParameter<int> chan1;
+	ofParameter<int> chan2;
+	ofParameter<int> chan3;
 	ofParameter<int> chan4;
-    ofParameter<int> chan5;
-    ofParameter<int> chan6;
+	ofParameter<int> chan5;
+	ofParameter<int> chan6;
 	ofParameter<int> chan7;
-    ofParameter<int> chan8;
-    ofParameter<int> chan9;
+	ofParameter<int> chan8;
+	ofParameter<int> chan9;
 	ofParameter<int> chan10;
-    ofParameter<int> chan11;
-    ofParameter<int> chan12;
-	ofParameter<int> chan13;
-    ofParameter<int> chan14;
-    ofParameter<int> chan15;
-	ofParameter<int> chan16;
+	ofParameter<int> chan11;
+	ofParameter<int> chan12;
+//	ofParameter<int> chan13;
+//    ofParameter<int> chan14;
+//    ofParameter<int> chan15;
+//	ofParameter<int> chan16;
 
 
     ofParameter<bool> autoCycle;
